1bd7d4bf6a5c0742bc97f9ec4f95ceeff2512bc9
BugHunter-X <askzenai@gmail.com>
2025-01-28 05:08:06 +0100

    Add Bug.cs file

---
+++ Bug.cs
@@ -0,0 +1,9 @@
+public class ExampleClass{
+    public int MyProperty { get; set; }
+
+    public void MyMethod()
+    {
+        // This line will cause a NullReferenceException if MyProperty is not initialized
+        Console.WriteLine(MyProperty.ToString()); 
+    }
+}
